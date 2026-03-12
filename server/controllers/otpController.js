d892eff79547ae3ffa40680c221e381e4fefd0f6
Akash-Jain <aj0881871@gmail.com>
2026-03-12 16:17:49 +0530

    add the produtcion

--- server/controllers/otpController.js
+++ server/controllers/otpController.js
@@ -32,6 +32,9 @@ export const createOTP = async (req,res)=>{
    if(process.env.NODE_ENV === "development"){
       console.log("OTP created:", otp);
    }
+   if(process.env.NODE_ENV === "production"){
+      console.log("OTP created:", otp);
+   }
 
    res.json({message:"OTP generated",success:true});
 }
