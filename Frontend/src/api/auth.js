f760fcc3f7bad95230331cc3de5c389ab379ad91
Akash-Jain <aj0881871@gmail.com>
2026-03-14 00:40:37 +0530

    last

--- Frontend/src/api/auth.js
+++ Frontend/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smart-secured-locker.onrender.com/api",
+  baseURL: "https://smart-secured-locker.onrender.com/api"||import.meta.env.VITE_API_URL,
 });
 
 // SIGNUP
