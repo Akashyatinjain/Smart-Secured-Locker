3a9e29a4d0ab0270353e6571663c5f8cba2d4abe
Akash-Jain <aj0881871@gmail.com>
2026-03-14 12:26:27 +0530

    serbver

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const limiter = rateLimit({
 app.disable("x-powered-by");
 
 app.use(cors({
-  origin: ["http://localhost:5173","https://smart-secured-locker-1fz5h74ni-akashyatinjains-projects.vercel.app"],
+  origin: ["http://localhost:5173","https://smart-secured-locker.vercel.app"],
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
   credentials: true
 }));
